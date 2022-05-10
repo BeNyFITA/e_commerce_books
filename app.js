485c3f660902ee8fc469ebf350be192c78c0d2c9
Johannes <johannes001002@gmail.com>
2022-05-10 20:37:26 -0300

    alterando pastas

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const app = express();
+const home = require(__dirname + '\\routes\\index')
 
 app.set('view engine', 'ejs')
+app.use(express.static(__dirname + 'public'))
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+})
+
+app.use('/', home);
