da5dd1b9e5258f6bd9da5d61455820e16f5d4275
Johannes <johannes001002@gmail.com>
2022-05-27 18:57:30 -0300

    validacoes

--- routes/cadastro.routes.js
+++ routes/cadastro.routes.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const router = express.Router();
 const cadastro = require('../controllers/cadastroController')
+const {check, validationResult } = require('express-validator')
 
 router.get('/criar', cadastro.registro)
-router.post("/criar", cadastro.criarRegistro)
+// Abaixo está sendo realizado as validações da rota criar (adicionais de segurança)
+const validacoes = [
+    check('nome').isEmpty(),
+    check('sobrenome').isEmpty(),
+    check('email').isEmail(),
+    check('cpf').isLength({min: 11, max: 11}),
+    check('senha').isStrongPassword(),
+    check('data').isDate(),
+]
+
+const verificarErros = (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        console.log(res.status(400))
+    }
+}
+
+router.post("/criar", validacoes ,cadastro.criarRegistro)
 router.get('/cadastroFeito', cadastro.guardarRegistro)
 
 module.exports = router;
